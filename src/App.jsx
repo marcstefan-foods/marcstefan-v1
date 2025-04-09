a160cd11cc0b3dfa9c06ce896a3006fefa5c2609
Ceeminsola <ceemin19@gmail.com>
2025-04-09 21:55:29 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100 text-4xl font-bold text-orange-500 ">
+      <div className="bg-white p-10 rounded-lg shadow-lg ">
+      <p className="animate-pulse">Welcome to MarcStefan Foods</p>
+      <p className="text-2xl text-center text-green-800 mt-8 animate-bounce">We will start building in a bit</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
